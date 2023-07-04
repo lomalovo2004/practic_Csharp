7ef708806b735ea374d32320b0026127b1a67394
lomalovo2004 <shulgae2004@gmail.com>
2023-07-04 19:36:54 +0300

    practica
    C#

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ namespace StudentClass
     {
         static void Main(string[] args)
         {
-            // Создание объекта класса Student с помощью конструктора
+
             Student student1 = new Student("Иванов", "Иван", "Иванович", "М8О", 113, 2023, "C#");
 
-            // Вывод значений полей объекта student1
+
             Console.WriteLine($"Фамилия: {student1.Surname}");
             Console.WriteLine($"Имя : {student1.Name}");
             Console.WriteLine($"Отчество : {student1.Secondname}");
@@ -20,7 +20,7 @@ namespace StudentClass
             Console.WriteLine($"Курс: {student1.Kurs(student1)}");
             
 
-            // Создание объекта класса Student с помощью свойств и модификация значения поля
+
             Student student2 = new Student();
             student2.Surname = "Петров";
             student2.Name = "Петр";
@@ -30,7 +30,7 @@ namespace StudentClass
             student2.YearOfAdmission = 2022;
             student2.Practic = "C#";
 
-            // Вывод значений полей объекта student2
+
             Console.WriteLine($"Фамилия: {student2.Surname}");
             Console.WriteLine($"Имя : {student2.Name}");
             Console.WriteLine($"Отчество : {student2.Secondname}");
@@ -41,7 +41,7 @@ namespace StudentClass
             Console.WriteLine($"Курс практики : {student2.Practic}");
             Console.WriteLine($"Курс: {student2.Kurs(student2)}");
 
-            // Проверка метода Equals
+
             Console.WriteLine($"Равны ли студенты? {student1.Equals(student2)}");
         }
     }
@@ -58,7 +58,7 @@ namespace StudentClass
         private string practic;
 
 
-        // Конструктор объекта класса
+
         public Student(string Surname, string Name, string Secondname, string univer, int group, int yearOfAdmission, string Practic)
         {
             this.Surname = Surname;
@@ -75,12 +75,12 @@ namespace StudentClass
             this.Practic = Practic ?? throw new ArgumentNullException(nameof(Practic));
         }
 
-        // Пустой конструктор объекта класса
+
         public Student()
         {
         }
 
-        // Свойства для доступа к полям
+
         public string Surname
         {
             get { return surname; }
@@ -125,7 +125,7 @@ namespace StudentClass
 
 
 
-        // Метод для проверки равенства объектов класса Student
+
         public bool Equals(Student other)
         {
             if (other == null)
